e80e06a1ab40020fed37e6acee3ba2dd6022b74d
Vitalina Krasnoplakhtich <krasnoplakhtich.vitalina@gmail.com>
2024-05-21 17:24:09 +0300

    Change deleteAction

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -35,7 +35,7 @@ const contactsSlice = createSlice({
       .addCase(deleteContact.pending, handlePending)
       .addCase(deleteContact.fulfilled, (state, action) => {
         state.loading = false;
-        state.items.filter((el) => el.id !== action.payload.id);
+        state.items = state.items.filter((el) => el.id !== action.payload.id);
       })
       .addCase(deleteContact.rejected, handleRejected),
 });
